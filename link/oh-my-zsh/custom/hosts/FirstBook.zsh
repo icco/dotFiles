935bf498e5e4cb33fc8c6910aa0dd0c3af447d51
Nat Welch <nat@natwelch.com>
2018-09-05 18:43:28 +0000

    Fix a few things

--- link/oh-my-zsh/custom/hosts/FirstBook.zsh
+++ link/oh-my-zsh/custom/hosts/FirstBook.zsh
@@ -24,6 +24,7 @@ export LSCOLORS=ExFxCxDxBxegedabagacad
 # Golang
 export GOPATH="$HOME/Projects"
 export PATH="$GOPATH/bin:$PATH"
+export GO111MODULE="on"
 
 # RVM
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
