1ce827c5aa377617ab3a7b6b86da34562883cea1
Nat Welch <nat@natwelch.com>
2019-07-10 15:57:13 -0400

    storm

--- link/oh-my-zsh/custom/hosts/storm.zsh
+++ link/oh-my-zsh/custom/hosts/storm.zsh
@@ -1,7 +1,5 @@
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
-[ -f ~/.fzf.bash ] && source ~/.fzf.bash
-
 # Golang
 export GOPATH=~/Projects/
 export PATH="~/Projects/bin:$PATH"
@@ -14,5 +12,7 @@ export NVM_DIR="$HOME/.nvm"
 PINENTRY_USER_DATA="USE_CURSES=1"
 GPG_TTY=$(tty)
 export GPG_TTY
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 . /home/nat/Projects/z/z.sh
