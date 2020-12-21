e234ff8240589b77d71ed0d2c9fac47085b0ffa3
Nat Welch <nat@natwelch.com>
2020-12-20 21:22:01 -0500

    piaware

---
+++ link/oh-my-zsh/custom/hosts/piaware.zsh
@@ -0,0 +1,18 @@
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+
+# Golang
+export GOPATH=~/Projects/
+export PATH="$GOPATH/bin:$PATH"
+export GO111MODULE=on
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+source /usr/share/doc/fzf/examples/key-bindings.zsh
+
+# python
+export PATH="$PATH:$HOME/.local/bin"
+
+. /home/nat/Projects/z/z.sh
