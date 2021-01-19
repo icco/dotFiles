6e49504bb58a34321d18720704d8a6b6df091c1e
Nat Welch <nat@natwelch.com>
2021-01-19 20:42:36 +0000

    wind

---
+++ link/oh-my-zsh/custom/hosts/wind.zsh
@@ -0,0 +1,4 @@
+export GOPATH="$HOME/Projects"
+export PATH="$GOPATH/bin:$PATH"
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
