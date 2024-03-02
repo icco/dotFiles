bbe63a06faced8ac579234d725cfd7d2cd8657dc
Nat Welch <nat@natwelch.com>
2024-03-02 20:26:00 +0000

    norns

---
+++ link/oh-my-zsh/custom/hosts/norns.zsh
@@ -0,0 +1,5 @@
+export GOPATH="$HOME/Projects"
+export PATH="$GOPATH/bin:$PATH"
+export GO111MODULE="on"
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
