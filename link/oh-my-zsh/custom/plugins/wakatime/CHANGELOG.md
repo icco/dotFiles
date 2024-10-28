9e32ba4dd5744b702141f1ec4ce026a5da9fc27c
Nat Welch <nat@natwelch.com>
2024-10-27 20:06:01 -0400

    sure

---
+++ link/oh-my-zsh/custom/plugins/wakatime/CHANGELOG.md
@@ -0,0 +1,49 @@
+# Changelog
+
+
+## Version 0.2.0
+
+### Improvements
+
+- Addresses review from Reddit: removes useless functions, improves performance
+
+### Bugfixes
+
+- Fixes how `should_work_offline` work
+
+
+## Version 0.1.1
+
+### Improvements
+
+- Linting fixes
+
+
+## Version 0.1.0
+
+### Improvements
+
+- Bumps `wakatime` to `12.0`
+- Adds `$WAKATIME_TIMEOUT` option
+- Adds `$WAKATIME_DISABLE_OFFLINE` option
+
+### Bugfixes
+
+- Now exceptions from `wakatime` are visible in the console
+
+
+## Version 0.0.2
+
+### Improvements
+
+- Now calls to `wakatime` server are async
+- If `wakatime` cli is not installed, plugin will tell users about it
+
+### Bugfixes
+
+- Fixed the bug with `$WAKATIME_DO_NOT_TRACK` killing the shell
+
+
+## Version 0.0.1
+
+- Initial release
---
+++ link/oh-my-zsh/custom/plugins/wakatime/wakatime.plugin.zsh
@@ -0,0 +1,66 @@
+# wakatime-zsh-plguin
+#
+# Documentation is available at:
+# https://github.com/sobolevn/wakatime-zsh-plugin
+
+_wakatime_heartbeat() {
+  # Sends a heartbeat to the wakarime server before each command.
+  # But it can be disabled by an environment variable:
+  # Set `$WAKATIME_DO_NOT_TRACK` to 1 to skip the tracking.
+  if (( WAKATIME_DO_NOT_TRACK )); then
+    return  # Tracking is skipped!
+  fi
+
+  # Checks if `wakatime` is installed,
+  # syntax is `zsh` specific, see: https://unix.stackexchange.com/a/237084
+  if (( ! $+commands[wakatime] )); then
+    echo 'wakatime cli is not installed, run:'
+    echo '$ pip install wakatime'
+    echo 'Or check that wakatime is in PATH'
+    echo
+    echo 'Time is not tracked for now.'
+    return
+  fi
+
+  # We only send the last command to the wakatime.
+  # We only send the first argument, which is a binary in 99% of cases.
+  # It does not include any sensitive information.
+  local last_command
+  last_command=$(echo "$1" | cut -d ' ' -f1)
+
+  # We only take the `root` directory name.
+  # We detect `root` directories by `.git` folder.
+  # If we are not in the git repository,
+  # take the default `Terminal` project.
+  local root_directory
+  root_directory=$(
+    git rev-parse --show-toplevel 2>/dev/null || echo 'Terminal'
+  )
+
+  # Checks if the app should work online, otherwise returns
+  # a special option to turn `wakatime` sync off:
+  local should_work_online
+  if (( WAKATIME_DISABLE_OFFLINE )); then
+    should_work_online='--disable-offline'
+  else
+    should_work_online=''
+  fi
+
+  wakatime --write \
+    --plugin 'wakatime-zsh-plugin/0.2.1' \
+    --entity-type app \
+    --entity "$last_command" \
+    --project "${root_directory:t}" \
+    --language sh \
+    --timeout "${WAKATIME_TIMEOUT:-5}" \
+    $should_work_online \
+    &>/dev/null </dev/null &!
+}
+
+# See docs on `add-zsh-hook`:
+# https://github.com/zsh-users/zsh/blob/master/Functions/Misc/add-zsh-hook
+autoload -U add-zsh-hook
+
+# See docs on what `preexec` is:
+# http://zsh.sourceforge.net/Doc/Release/Functions.html
+add-zsh-hook preexec _wakatime_heartbeat
