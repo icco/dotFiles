977d9cd4af97927a49b774560153645ab8df44e8
Nat Welch <nat@natwelch.com>
2025-04-18 14:11:02 -0400

    brook

---
+++ link/oh-my-zsh/custom/hosts/brook.zsh
@@ -0,0 +1,28 @@
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+
+# Golang
+export GOPATH=~/Projects/
+export PATH="$GOPATH/bin:$PATH"
+export GO111MODULE=on
+
+export PATH="/snap/bin:$PATH"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# GPG
+PINENTRY_USER_DATA="USE_CURSES=1"
+GPG_TTY=$(tty)
+export GPG_TTY
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+source /usr/share/doc/fzf/examples/key-bindings.zsh
+
+# python
+export PATH="$PATH:$HOME/.local/bin"
+
+# zmv
+autoload -Uz zmv
+
+# vim: set filetype=zsh:
