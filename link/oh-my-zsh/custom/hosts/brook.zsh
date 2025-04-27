29092298f609eb05d7f4b5789263f5c002eaeebe
Nat Welch <nat@natwelch.com>
2025-04-26 22:09:53 -0400

    chore: add fd

--- link/oh-my-zsh/custom/hosts/brook.zsh
+++ link/oh-my-zsh/custom/hosts/brook.zsh
@@ -7,6 +7,8 @@ export GO111MODULE=on
 
 export PATH="/snap/bin:$PATH"
 
+alias fd=fdfind
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
