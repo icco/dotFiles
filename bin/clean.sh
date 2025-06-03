09c1ab3bda9132f1c099ba0275049b8b13a5ade2
Nat Welch <nat@natwelch.com>
2025-06-03 02:58:52 +0000

    update

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,9 +1,13 @@
 #! /bin/zsh
 
-set -ex
+set -x
 
 rm -rf ~/Work/*/node_modules
 rm -rf ~/Work/**/.terraform
 
 rm -rf ~/Projects/*/node_modules
 rm -rf ~/Projects/**/.terraform
+
+sudo rm -rf $GOPATH/pkg/
+
+docker system prune -af
