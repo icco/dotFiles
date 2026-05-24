bc150c562e10c1757c2eafc7a257745f58761f16
Nat Welch <nat@natwelch.com>
2026-05-24 02:04:26 +0000

    fix

---
+++ link/oh-my-zsh/custom/hosts/localhost.zsh
@@ -0,0 +1,23 @@
+export GOPATH=~/Projects/
+export PATH="$GOPATH/bin:$PATH"
+export GO111MODULE=on
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# GPG
+PINENTRY_USER_DATA="USE_CURSES=1"
+GPG_TTY=$(tty)
+export GPG_TTY
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+source /usr/share/doc/fzf/examples/key-bindings.zsh
+
+# python
+export PATH="$PATH:$HOME/.local/bin"
+
+# zmv
+autoload -Uz zmv
+
+# vim: set filetype=zsh:
