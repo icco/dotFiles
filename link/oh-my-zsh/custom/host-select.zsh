ef0fe66f8b5e0e0a66ea88ee97cb2f0e5488753d
Nat Welch <nat@natwelch.com>
2026-05-24 16:59:52 +0000

    termux support

--- link/oh-my-zsh/custom/host-select.zsh
+++ link/oh-my-zsh/custom/host-select.zsh
@@ -2,6 +2,14 @@ custom=${ZSH}/custom/hosts
 
 # strip router bullshit
 host=$(hostname | sed 's/\./ /g' | awk '{ print $1 }')
+
+# Termux on Android reports "localhost"; ask the system for the real name.
+if [[ "$host" == "localhost" ]] && (( $+commands[getprop] )); then
+   real=$(getprop net.hostname 2>/dev/null)
+   [[ -z "$real" ]] && real=$(getprop ro.product.model 2>/dev/null | tr ' ' '_')
+   [[ -n "$real" ]] && host=$real
+fi
+
 if [[ -f $custom/$host.zsh ]]; then
    . $custom/$host.zsh
    echo "===> Loaded $host.zsh";
