c74345ca1062fe236c7cfd4cd7b200aaff7dbf85
Nat Welch <nat@natwelch.com>
2026-05-24 17:02:02 +0000

    simplify

--- link/oh-my-zsh/custom/host-select.zsh
+++ link/oh-my-zsh/custom/host-select.zsh
@@ -6,11 +6,12 @@ host=$(hostname | sed 's/\./ /g' | awk '{ print $1 }')
 # Termux on Android reports "localhost"; ask the system for the real name.
 if [[ "$host" == "localhost" ]] && (( $+commands[getprop] )); then
    real=$(getprop net.hostname 2>/dev/null)
-   [[ -z "$real" ]] && real=$(getprop ro.product.model 2>/dev/null | tr ' ' '_')
    [[ -n "$real" ]] && host=$real
 fi
 
 if [[ -f $custom/$host.zsh ]]; then
    . $custom/$host.zsh
    echo "===> Loaded $host.zsh";
+ else
+   echo "!!! $custom/$host.zsh does not exist"
 fi
