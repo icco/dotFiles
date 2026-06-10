fae9dcbaa57523912fd66c6bd2a686bc552b3278
Nat Welch <nat@natwelch.com>
2026-06-10 14:01:31 +0000

    update clean

--- bin/clean.sh
+++ bin/clean.sh
@@ -3,10 +3,14 @@
 set -x
 
 rm -rf ~/Work/*/node_modules
+rm -rf ~/Work/**/node_modules
 rm -rf ~/Work/**/.terraform
 
 rm -rf ~/Projects/*/node_modules
 rm -rf ~/Projects/**/.terraform
+
+rm -rf ~/Projects/**/.next
+rm -rf ~/Work/**/.next
 
 yarn cache clean
 npm cache clean --force
